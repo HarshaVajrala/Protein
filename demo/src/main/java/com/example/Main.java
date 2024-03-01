886477a07c185a62806e8ecb0bc1c0b5b87023d6
Will-I-Am-Ham <wh89578@gmail.com>
2024-02-29 16:03:09 -0800

    actually got it working <3

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,9 +1,19 @@
 package com.example;
-import jsoup.*;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-    
+        String html = "<html><head><title>Sample Title</title></head>"
+        + "<body><p>Sample Content</p></body></html>";
+        Document document = Jsoup.parse(html);
+        System.out.println(document.title());
+        Elements paragraphs = document.getElementsByTag("p");
+        for (Element paragraph : paragraphs) {
+            System.out.println(paragraph.text());
+        }
     }
 }
